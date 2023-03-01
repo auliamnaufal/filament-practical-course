2ba6ee42e474931ceb3297a36a584d5c36cc1ee9
auliamnaufal <auliamnaufal@gmail.com>
2023-03-01 22:06:53 +0700

    Feature: add multi select input at ProductResource.php filled with tags table

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -41,7 +41,10 @@ class ProductResource extends Resource
                     ->rule('max:100'),
                 Forms\Components\TextInput::make('slug')->required(),
                 Forms\Components\TextInput::make('price')->required()->numeric(),
-                Forms\Components\FileUpload::make('image')
+                Forms\Components\FileUpload::make('image'),
+                Forms\Components\Select::make('tags')
+                    ->multiple()
+                    ->relationship('tags', 'name')
             ]);
     }
 
