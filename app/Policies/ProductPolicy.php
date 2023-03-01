158fd5b4d0b0fbdd7e5dfb3095c1d2162ffa6ccd
auliamnaufal <auliamnaufal@gmail.com>
2023-03-01 22:18:45 +0700

    Feature: generate policy for accessing user and product only for admin user using laravel policies

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ProductPolicy
+{
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->role_id == 2;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Product $product): bool
+    {
+        return $user->role_id == 2;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Product $product): bool
+    {
+        return $user->role_id == 2;
+    }
+
+    public function deleteAny(User $user) {
+        return $user->role_id == 2;
+    }
+
+}
